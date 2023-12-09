4e25a0b46a6e979398a402de148393e260b2a294
mohtzz94@gmail.com <admin@Rachels-MacBook.local>
2023-12-09 15:15:19 +0630

    first commit added files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <FirstComponentVue></FirstComponentVue>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <SecondComponentVue></SecondComponentVue>
+
+</template>
+
+<script>
+import FirstComponentVue from './components/FirstComponent.vue'
+import SecondComponentVue from './components/SecondComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FirstComponentVue,
+    SecondComponentVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>First Component</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente, doloribus est? Quos nulla architecto vero eveniet facere exercitationem. Nostrum molestias culpa odit eaque voluptatum laudantium. Velit itaque voluptatem facilis fugit.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FirstComponent"
+    }
+</script>
+
+<style scoped>
+
+</style>
